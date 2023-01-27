dd1f82b285bc61b63531b7f2bdc85fb9605ad402
notKamui <teillard.jimmy@outlook.fr>
2023-01-27 16:34:45 +0100

    Extracted test initialization

--- app/src/androidTest/java/fr/uge/plutus/backend/TransactionsTest.kt
+++ app/src/androidTest/java/fr/uge/plutus/backend/TransactionsTest.kt
@@ -20,9 +20,12 @@ class TransactionsTest {
     private lateinit var db: Database
     private lateinit var transactionDao: TransactionDao
     private lateinit var bookDao: BookDao
+    private lateinit var book: Book
+    private lateinit var book2: Book
 
     @Before
-    fun init() {
+    @OptIn(ExperimentalCoroutinesApi::class)
+    fun init() = runTest {
         val context = ApplicationProvider.getApplicationContext<Context>()
         db = Room.inMemoryDatabaseBuilder(
             context,
@@ -30,17 +33,17 @@ class TransactionsTest {
         ).build()
         transactionDao = db.transactions()
         bookDao = db.books()
+        val b = Book("Android For Dummies")
+        bookDao.insert(b)
+        book = b
+        val b2 = Book("Kotlin For Dummies")
+        bookDao.insert(b2)
+        book2 = b2
     }
 
     @Test
     @OptIn(ExperimentalCoroutinesApi::class)
     fun shouldCreateATransactionWithoutFailing() = runTest {
-        val book = Book("Android For Dummies")
-        bookDao.insert(book)
-
-        val bookFromDB = bookDao.findById(book.uuid)
-        assertEquals(book, bookFromDB)
-
         val transaction = Transaction("First Transaction", Date(0), 10.0, book.uuid)
         transactionDao.insert(transaction)
 
@@ -66,19 +69,14 @@ class TransactionsTest {
     @Test
     @OptIn(ExperimentalCoroutinesApi::class)
     fun shouldGetAllBookTransactions() = runTest {
-        val book = Book("Android For Dummies")
-        val book2 = Book("Kotlin For Dummies")
-        bookDao.insert(book)
-        bookDao.insert(book2)
-
-        val books = bookDao.getAll()
-        assertEquals(2, books.size)
-        assertTrue(book in books)
-        assertTrue(book2 in books)
-
         val transaction1 = Transaction("First Transaction", Date(0), 10.0, book.uuid)
         val transaction2 = Transaction("Second Transaction", Date(10), 12.2, book.uuid)
-        val transaction3 = Transaction("First Transaction of another book", Date(5), 7.85, book2.uuid)
+        val transaction3 = Transaction(
+            "First Transaction of another book",
+            Date(5),
+            7.85,
+            book2.uuid
+        )
 
         transactionDao.insert(transaction1)
         transactionDao.insert(transaction2)
@@ -93,11 +91,6 @@ class TransactionsTest {
     @Test
     @OptIn(ExperimentalCoroutinesApi::class)
     fun shouldUpdateATransaction() = runTest {
-        val book = Book("Android For Dummies")
-        bookDao.insert(book)
-        val bookFromDB = bookDao.findById(book.uuid)
-        assertEquals(book, bookFromDB)
-
         val transaction = Transaction("Second Transaction", Date(0), 10.0, book.uuid)
         transactionDao.insert(transaction)
         val transactionFromDb = transactionDao.findById(transaction.id)!!
@@ -112,11 +105,6 @@ class TransactionsTest {
     @Test
     @OptIn(ExperimentalCoroutinesApi::class)
     fun shouldDeleteATransaction() = runTest {
-        val book = Book("Android For Dummies")
-        bookDao.insert(book)
-        val bookFromDB = bookDao.findById(book.uuid)
-        assertEquals(book, bookFromDB)
-
         val transaction = Transaction("First Transaction", Date(0), 10.0, book.uuid)
         transactionDao.insert(transaction)
         val transactionFromDB = transactionDao.findById(transaction.id)!!
@@ -130,13 +118,9 @@ class TransactionsTest {
     @Test
     @OptIn(ExperimentalCoroutinesApi::class)
     fun shouldDeleteTransactionOnCascade() = runTest {
-        val book = Book("Android For Dummies")
-        bookDao.insert(book)
         val transaction = Transaction("First Transaction", Date(0), 10.0, book.uuid)
         transactionDao.insert(transaction)
 
-        val bookFromDB = bookDao.findById(book.uuid)
-        assertEquals(book, bookFromDB)
         val transactionFromDB = transactionDao.findById(transaction.id)
         assertEquals(transaction, transactionFromDB)
 
