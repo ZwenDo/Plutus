90db69cd5934430280e0a57c78bf832847785cd7
Irwin Madet <irwin.madet@gmail.com>
2023-02-09 14:01:25 +0100

    Add persistence logic to book creation view
    Co-authored-by: notKamui <teillard.jimmy@outlook.fr>

--- app/src/main/java/fr/uge/plutus/MainActivity.kt
+++ app/src/main/java/fr/uge/plutus/MainActivity.kt
@@ -7,12 +7,15 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.ui.Modifier
+import fr.uge.plutus.backend.Database
 import fr.uge.plutus.frontend.view.MainView
 import fr.uge.plutus.ui.theme.PlutusTheme
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        Database.init(this)
         setContent {
             PlutusTheme {
                 // A surface container using the 'background' color from the theme
--- app/src/main/java/fr/uge/plutus/frontend/view/BookCreationView.kt
+++ app/src/main/java/fr/uge/plutus/frontend/view/BookCreationView.kt
@@ -1,19 +1,21 @@
 package fr.uge.plutus.frontend.view
 
+import android.database.sqlite.SQLiteConstraintException
+import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import fr.uge.plutus.backend.Book
+import fr.uge.plutus.backend.Database
 import fr.uge.plutus.frontend.component.form.InputText
 
 @Preview(showBackground = true)
@@ -22,13 +24,28 @@ fun BookCreatorPreview() {
     BookCreationView()
 }
 
-data class BookDTO(val name: String)
-
 @Composable
-fun BookCreationView(
-    onBookCreated: (BookDTO) -> Unit = {}
-) {
+fun BookCreationView(onExit: () -> Unit = {}) {
+    val context = LocalContext.current
+
     var bookName by rememberSaveable { mutableStateOf("") }
+    var creating by rememberSaveable { mutableStateOf(false) }
+    var errorMessage by rememberSaveable { mutableStateOf<String?>(null) }
+
+    LaunchedEffect(creating) {
+        if (!creating) return@LaunchedEffect
+
+        val book = Book(bookName)
+        try {
+            Database.books().insert(book)
+            Toast.makeText(context, "Book “${book.name}” created", Toast.LENGTH_SHORT).show()
+            onExit()
+        } catch (e: SQLiteConstraintException) {
+            errorMessage = "Book already exists"
+        }
+
+        creating = false
+    }
 
     Box(
         modifier = Modifier
@@ -46,11 +63,12 @@ fun BookCreationView(
                     .weight(1f),
                 verticalArrangement = Arrangement.Center
             ) {
-                InputText(label = "Book name", value = bookName, onValueChange = { bookName = it })
+                InputText(label = "Book name", value = bookName, errorMessage = errorMessage) {
+                    bookName = it
+                    errorMessage = null
+                }
             }
-            Button(modifier = Modifier.fillMaxWidth(), onClick = {
-                onBookCreated(BookDTO(bookName))
-            }) {
+            Button(modifier = Modifier.fillMaxWidth(), onClick = { creating = true }) {
                 Text(text = "CREATE", fontWeight = FontWeight.SemiBold)
             }
         }
