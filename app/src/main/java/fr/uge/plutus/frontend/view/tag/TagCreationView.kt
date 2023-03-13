8e06ade32cda67b6b64aaafb5d95aa4db861ec69
FxTsuna <FredericXu@outlook.fr>
2023-03-13 19:16:15 +0100

    Able to add multiple tag on a transaction

--- app/src/main/java/fr/uge/plutus/frontend/view/tag/TagCreationView.kt
+++ app/src/main/java/fr/uge/plutus/frontend/view/tag/TagCreationView.kt
@@ -1,10 +1,10 @@
 package fr.uge.plutus.frontend.view.tag
 
 import android.database.sqlite.SQLiteConstraintException
+import androidx.compose.runtime.*
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
 import androidx.compose.material.*
-import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -34,40 +34,51 @@ private suspend fun updateTagMap(book: Book): Map<String, Tag> =
         return@withContext tagsValue.associateBy { it.name!! }
     }
 
+private suspend fun createTag(book: Book, name: String, transaction: Transaction) =
+    withContext(Dispatchers.IO) {
+        val tag = Database.tags().insert(name, book.uuid)
+        Database.tagTransactionJoin().insert(transaction, tag)
+    }
+
 @Composable
 fun TagCreationView(onExit: () -> Unit) {
     val currentBook = GlobalState.currentBook!!
     val currentTransaction = GlobalState.currentTransaction!!
     val context = LocalContext.current
     var isOpen by rememberSaveable { mutableStateOf(false) }
-    var tagName by rememberSaveable { mutableStateOf("") }
+    var creatingTag by rememberSaveable { mutableStateOf("") }
     var creating by rememberSaveable { mutableStateOf(false) }
     var errorMessage by rememberSaveable { mutableStateOf<String?>(null) }
     var tagMap by rememberSaveable { mutableStateOf(emptyMap<String, Tag>()) }
-    var selectedTag by rememberSaveable { mutableStateOf("") }
     var loaded by rememberSaveable { mutableStateOf(false) }
-
+    val tagToAdd = rememberSaveable { mutableStateListOf<Tag>() }
 
     LaunchedEffect(creating) {
         if (!creating) return@LaunchedEffect
 
-        if (tagName.isBlank()) {
-            errorMessage = "Tag name cannot be empty"
+        if (tagToAdd.isEmpty()) {
+            errorMessage = "You must select at least one tag"
             creating = false
             return@LaunchedEffect
         }
 
-        try {
-            withContext(Dispatchers.IO) {
-                val tag = Database.tags().insert(tagName, currentBook.uuid)
-                Database.tagTransactionJoin().insert(currentTransaction, tag)
+        if (creatingTag.isNotBlank()) {
+            try {
+                createTag(currentBook, creatingTag, currentTransaction)
+                Toast.makeText(context, "Tag created", Toast.LENGTH_SHORT).show()
+                onExit()
+            } catch (e: SQLiteConstraintException) {
+                errorMessage = "Tag name already exist"
             }
-            Toast.makeText(context, "Tag created", Toast.LENGTH_SHORT).show()
-            onExit()
-        } catch (e: SQLiteConstraintException) {
-            errorMessage = "Tag name already exist"
         }
 
+        for (tag in tagToAdd) {
+            withContext(Dispatchers.IO) {
+                Database.tagTransactionJoin().insert(currentTransaction, tag)
+            }
+        }
+        Toast.makeText(context, "Tag added", Toast.LENGTH_SHORT).show()
+        tagToAdd.clear()
         creating = false
     }
 
@@ -91,15 +102,18 @@ fun TagCreationView(onExit: () -> Unit) {
                 Column(
                     modifier = Modifier.padding(16.dp)
                 ) {
-                    InputText(label = "Create a new tag", value = tagName) { tagName = it }
+                    InputText(
+                        label = "Create a new tag",
+                        value = creatingTag
+                    ) { creatingTag = it }
                     Box(modifier = Modifier.wrapContentSize()) {
                         InputSelectCollection(
                             label = "Select an existing tag",
                             options = tagMap.values.map { it.name!! },
-                            initial = selectedTag,
+                            initial = "",
                             mapFromString = { tagMap[it]!! },
                             mapToString = { it.toString() },
-                            onSelected = { selectedTag = it.toString() }
+                            onSelected = { tagMap[it]?.let { tag -> tagToAdd += tag } }
                         )
                     }
                     Row(
@@ -111,7 +125,6 @@ fun TagCreationView(onExit: () -> Unit) {
                     ) {
                         Button(onClick = {
                             creating = true
-                            isOpen = false
                             loaded = false
                         }) {
                             Text(text = "ADD", fontWeight = FontWeight.SemiBold)
