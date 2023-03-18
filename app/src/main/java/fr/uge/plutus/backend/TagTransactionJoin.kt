5cbf81f5d1f2cf87c9eb4100bc10e5eb5dc264b1
ZwenDo <lorris.creantor@gmail.com>
2023-03-18 18:13:11 +0100

    Replaced DAO abstract class by an interface

--- app/src/main/java/fr/uge/plutus/backend/TagTransactionJoin.kt
+++ app/src/main/java/fr/uge/plutus/backend/TagTransactionJoin.kt
@@ -28,7 +28,7 @@ data class TagTransactionJoin(
 )
 
 @Dao
-abstract class TagTransactionJoinDao {
+interface TagTransactionJoinDao {
 
     fun insert(transaction: Transaction, tag: Tag) {
         require(transaction.bookId == tag.bookId) { "Transaction and tag must belong to the same book" }
@@ -46,16 +46,16 @@ abstract class TagTransactionJoinDao {
     }
 
     @Delete
-    abstract fun _delete(tagTransactionJoin: TagTransactionJoin)
+    fun _delete(tagTransactionJoin: TagTransactionJoin)
 
     @androidx.room.Transaction
     @Query("SELECT * FROM tag JOIN tag_transaction_join ON tag.tagId = tag_transaction_join.tagId WHERE tag_transaction_join.transactionId = :transactionId")
-    abstract fun findTagsByTransactionId(transactionId: UUID): List<Tag>
+    fun findTagsByTransactionId(transactionId: UUID): List<Tag>
 
     @androidx.room.Transaction
     @Query("SELECT * FROM transactions JOIN tag_transaction_join ON transactions.transactionId = tag_transaction_join.transactionId WHERE tag_transaction_join.tagId = :tagId")
-    abstract fun findTransactionByTagId(tagId: UUID): List<Transaction>
+    fun findTransactionByTagId(tagId: UUID): List<Transaction>
 
     @Query("SELECT * FROM tag_transaction_join")
-    abstract fun findAll(): List<TagTransactionJoin>
+    fun findAll(): List<TagTransactionJoin>
 }
