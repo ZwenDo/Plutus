cc6f58f4158818e66880d32e8b20e4bd5f3ea0fe
ZwenDo <lorris.creantor@gmail.com>
2023-03-19 18:36:33 +0100

    Replaced attachment edit code by the extracted component

--- app/src/main/java/fr/uge/plutus/frontend/view/transaction/TransactionCreationView.kt
+++ app/src/main/java/fr/uge/plutus/frontend/view/transaction/TransactionCreationView.kt
@@ -1,45 +1,28 @@
 package fr.uge.plutus.frontend.view.transaction
 
-import android.content.Intent
 import android.database.sqlite.SQLiteConstraintException
-import android.net.Uri
 import android.os.Build
-import android.provider.MediaStore
+import android.util.Log
 import android.widget.Toast
-import androidx.activity.compose.rememberLauncherForActivityResult
-import androidx.activity.result.contract.ActivityResultContracts
 import androidx.annotation.RequiresApi
-import androidx.compose.foundation.border
-import androidx.compose.foundation.gestures.Orientation
-import androidx.compose.foundation.gestures.scrollable
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.rememberScrollState
 import androidx.compose.material.*
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Delete
 import androidx.compose.runtime.*
 import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import fr.uge.plutus.backend.*
 import fr.uge.plutus.backend.Currency
-import fr.uge.plutus.backend.Database
-import fr.uge.plutus.backend.TagType
-import fr.uge.plutus.backend.Transaction
 import fr.uge.plutus.frontend.component.form.InputDate
 import fr.uge.plutus.frontend.component.form.InputSelectEnum
 import fr.uge.plutus.frontend.component.form.InputText
-import fr.uge.plutus.frontend.store.GlobalState
-import fr.uge.plutus.ui.theme.Gray
 import fr.uge.plutus.frontend.store.globalState
+import fr.uge.plutus.frontend.view.attachment.AttachmentCreationView
 import fr.uge.plutus.util.toDateOrNull
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.withContext
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 import java.util.*
@@ -52,25 +35,48 @@ enum class Field {
 }
 
 @RequiresApi(Build.VERSION_CODES.O)
+@Preview(showBackground = true)
+@Composable
+private fun Preview() {
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = MaterialTheme.colors.background
+    ) {
+        TransactionCreationView()
+    }
+}
+
+@RequiresApi(Build.VERSION_CODES.O)
 @Composable
 fun TransactionCreationView(onExit: () -> Unit = {}) {
     val currentBook = globalState().currentBook
+    val initialTransaction = globalState().currentTransaction
     require(currentBook != null) { "No book selected" }
 
     val context = LocalContext.current
     var creating by rememberSaveable { mutableStateOf(false) }
     val errors = remember { mutableStateMapOf<Field, String>() }
 
-    var description by rememberSaveable { mutableStateOf("") }
-    var date by rememberSaveable { mutableStateOf("") }
-    var amount by rememberSaveable { mutableStateOf("") }
-    var currency by rememberSaveable { mutableStateOf(Currency.USD) }
-    val attachments = remember { mutableStateMapOf<Uri, String>() }
-    val launcher = rememberLauncherForActivityResult(
-        ActivityResultContracts.StartActivityForResult()
-    ) {
-        it.data?.data?.let { uri ->
-            attachments[uri] = uri.lastPathSegment ?: uri.toString()
+    var description by rememberSaveable { mutableStateOf(initialTransaction?.description ?: "") }
+    var date by rememberSaveable { mutableStateOf(initialTransaction?.date?.toString() ?: "") }
+    var amount by rememberSaveable { mutableStateOf(initialTransaction?.amount?.toString() ?: "") }
+    var currency by rememberSaveable {
+        mutableStateOf(initialTransaction?.currency ?: Currency.USD)
+    }
+
+    val initialAttachments = remember { mutableStateMapOf<UUID, Attachment>() }
+    val attachments = remember { mutableStateMapOf<UUID, Attachment>() }
+
+    LaunchedEffect(Unit) {
+        if (initialTransaction != null) {
+            withContext(Dispatchers.IO) {
+                val loaded = Database
+                    .attachments()
+                    .findAllByTransactionId(initialTransaction.transactionId)
+                    .associateBy { it.id }
+                initialAttachments += loaded
+                attachments += loaded
+            }
         }
     }
 
@@ -95,23 +101,36 @@ fun TransactionCreationView(onExit: () -> Unit = {}) {
         }
 
         try {
+            val transactionId = initialTransaction?.transactionId ?: UUID.randomUUID()
             val transaction = Transaction(
                 description = description,
                 date = actualDate!!,
                 amount = actualAmount!!,
                 currency = currency,
-                bookId = currentBook.uuid
+                bookId = currentBook.uuid,
+                transactionId = transactionId
             )
             withContext(Dispatchers.IO) {
-                Database.transactions().insert(transaction)
-                attachments.forEach { (uri, name) ->
-                    Database.attachments().insert(transaction, uri, name)
+                if (initialTransaction != null) {
+                    Database.transactions().update(transaction)
+                } else {
+                    Database.transactions().insert(transaction)
                 }
-            }
-            Database.transactions().insert(transaction)
-
-            withContext(Dispatchers.IO) {
-                if (transaction.date > Date()) {
+                attachments.forEach { (id, new) -> // for each attachment we have to check if it has been added, updated or deleted
+                    initialAttachments.compute(id) { _, old ->
+                        val toUpsert = new.copy(transactionId = transaction.transactionId)
+                        if (old == null) {
+                            Database.attachments()._insert(toUpsert)
+                        } else {
+                            Database.attachments().update(toUpsert)
+                        }
+                        null
+                    }
+                }
+                initialAttachments.forEach { (_, old) -> // delete the rest
+                    Database.attachments().delete(old)
+                }
+                if (initialTransaction != null && transaction.date > Date()) {
                     val tags = Database.tags()
                     val todoTag = tags
                         .findByName("@todo", currentBook.uuid)
@@ -183,77 +202,15 @@ fun TransactionCreationView(onExit: () -> Unit = {}) {
                         .height(8.dp)
                         .fillMaxWidth()
                 )
-                Button(
-                    modifier = Modifier.fillMaxWidth(),
-                    onClick = {
-                        val intent = Intent(
-                            Intent.ACTION_OPEN_DOCUMENT,
-                            MediaStore.Images.Media.EXTERNAL_CONTENT_URI
-                        )
-                            .apply {
-                                addCategory(Intent.CATEGORY_OPENABLE)
-                            }
-                        launcher.launch(intent)
-                    },
-                ) {
-                    Text(text = "Add attachment")
-                }
-                Column(
-                    Modifier
-                        .fillMaxWidth()
-                        .border(1.dp, Gray, MaterialTheme.shapes.small)
-                ) {
-                    LazyColumn(
-                        modifier = Modifier
-                            .scrollable(
-                                rememberScrollState(),
-                                orientation = Orientation.Vertical
-                            )
-                            .fillMaxWidth()
-                            .height(150.dp),
-                        content = {
-                            val iterator = attachments.iterator()
-                            items(attachments.size) { index ->
-                                val actualIndex = attachments.size - index - 1
-                                Row(verticalAlignment = Alignment.CenterVertically) {
-                                    val entry = iterator.next()
-                                    Button(
-                                        onClick = { attachments -= entry.key },
-                                        modifier = Modifier
-                                            .scale(0.5f)
-                                    ) {
-                                        Icon(Icons.Default.Delete, contentDescription = null)
-                                    }
-                                    TextField(
-                                        value = entry.value,
-                                        modifier = Modifier.fillMaxWidth(),
-                                        onValueChange = {
-                                            attachments[entry.key] = it.replace("\n", "")
-                                        },
-                                        singleLine = true,
-                                    )
-                                }
-                            }
-                        }
-                    )
-                }
+                AttachmentCreationView(attachments)
 
             }
             Button(modifier = Modifier.fillMaxWidth(), onClick = { creating = true }) {
-                Text(text = "CREATE", fontWeight = FontWeight.SemiBold)
+                Text(
+                    text = if (initialTransaction == null) "CREATE" else "SAVE",
+                    fontWeight = FontWeight.SemiBold
+                )
             }
         }
-    }
-}
-
-@RequiresApi(Build.VERSION_CODES.O)
-@Preview(showBackground = true)
-@Composable
-private fun Preview() {
-    Surface(
-        modifier = Modifier.fillMaxSize(),
-        color = MaterialTheme.colors.background
-    ) {
-        TransactionCreationView()
     }
 }
