ecb922192e3ef42f96f983b68b4c1eda4db1ac33
notKamui <teillard.jimmy@outlook.fr>
2023-03-20 21:14:58 +0100

    Added security config

---
+++ service/src/main/kotlin/fr/uge/plutus/plugins/Security.kt
@@ -0,0 +1,44 @@
+package fr.uge.plutus.plugins
+
+import io.ktor.server.auth.*
+import io.ktor.server.auth.jwt.*
+import com.auth0.jwt.JWT
+import com.auth0.jwt.algorithms.Algorithm
+import fr.uge.plutus.core.Environment
+import fr.uge.plutus.core.ExpiredOrInvalidTokenException
+import fr.uge.plutus.util.getHeader
+import io.ktor.server.application.*
+import java.lang.IllegalArgumentException
+
+fun Application.configureSecurity() {
+    authentication {
+        configureJWT("auth-jwt", Environment.jwtSecret) { call, _ ->
+            val token = call.getHeader("Authorization").replace("Bearer ", "")
+            val id = JWT.decode(token).subject ?: throw ExpiredOrInvalidTokenException()
+            BookFilePrincipal(id)
+        }
+    }
+}
+
+private fun AuthenticationConfig.configureJWT(
+    name: String,
+    secret: String,
+    block: (ApplicationCall, JWTCredential) -> Principal
+) = jwt(name) {
+    realm = Environment.jwtRealm
+
+    val jwtVerifier = JWT.require(Algorithm.HMAC256(secret)).build()
+    verifier(jwtVerifier)
+
+    validate { credential ->
+        kotlin.runCatching {
+            block(this, credential)
+        }.onFailure {
+            if (it !is IllegalArgumentException) throw it
+        }.getOrNull()
+    }
+
+    challenge { _, _ -> throw ExpiredOrInvalidTokenException() }
+}
+
+data class BookFilePrincipal(val id: String) : Principal
