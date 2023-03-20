7b28bf93bd07312a65b5a1ca70a05216e37493ad
notKamui <teillard.jimmy@outlook.fr>
2023-03-20 21:18:28 +0100

    Added app entry point

--- service/build.gradle.kts
+++ service/build.gradle.kts
@@ -5,7 +5,6 @@ val logback_version: String by project
 plugins {
     kotlin("jvm") version "1.8.10"
     id("io.ktor.plugin") version "2.2.4"
-    id("org.jetbrains.kotlin.plugin.serialization") version "1.8.10"
 }
 
 group = "fr.uge.plutus"
@@ -28,11 +27,7 @@ dependencies {
     implementation("io.ktor:ktor-server-auth-jwt-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-host-common-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-status-pages-jvm:$ktor_version")
-    implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktor_version")
-    implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktor_version")
     implementation("io.ktor:ktor-server-netty-jvm:$ktor_version")
     implementation("ch.qos.logback:logback-classic:$logback_version")
     implementation("io.ktor:ktor-server-config-yaml:$ktor_version")
-    testImplementation("io.ktor:ktor-server-tests-jvm:$ktor_version")
-    testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
 }
---
+++ service/src/main/kotlin/fr/uge/plutus/Application.kt
@@ -0,0 +1,16 @@
+package fr.uge.plutus
+
+import fr.uge.plutus.core.Environment
+import io.ktor.server.application.*
+import fr.uge.plutus.plugins.*
+import io.ktor.server.netty.*
+
+fun main(args: Array<String>): Unit = EngineMain.main(args)
+
+@Suppress("unused") // referenced by application.yaml
+fun Application.module() {
+    Environment.application = this
+    configureExceptionAdvisors()
+    configureSecurity()
+    configureRouting()
+}
