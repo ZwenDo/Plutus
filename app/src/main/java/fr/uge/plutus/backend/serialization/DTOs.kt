8420928404b6c4f6a67d6c53f70ef10d99eb116c
ZwenDo <lorris.creantor@gmail.com>
2023-03-20 15:19:50 +0100

    Started to add DTOs

--- app/src/main/java/fr/uge/plutus/backend/serialization/DTOs.kt
+++ app/src/main/java/fr/uge/plutus/backend/serialization/DTOs.kt
@@ -1,18 +1,29 @@
+@file:UseSerializers(UUIDSerializer::class)
 package fr.uge.plutus.backend.serialization
 
+import fr.uge.plutus.backend.*
 import java.util.UUID
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.UseSerializers
 
+@Serializable
 data class TransactionDTO(
     val transactionId: UUID,
-    val bookId: String,
     val description: String,
     val date: Long,
     val amount: Double,
-    val currency: String,
+    val currency: Currency,
     val latitude: Double?,
     val longitude: Double?
 )
+@Serializable
+data class TagDTO(
+    val tagId: UUID,
+    val name: String,
+    val type: TagType,
+)
 
+@Serializable
 data class BookDTO(
     val uuid: UUID,
     val name: String,
