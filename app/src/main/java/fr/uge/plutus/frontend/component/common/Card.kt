a051b3a62e5986d63b46ee5a42e0f3a49e95f740
notKamui <teillard.jimmy@outlook.fr>
2023-03-20 16:07:20 +0100

    Added card component

---
+++ app/src/main/java/fr/uge/plutus/frontend/component/common/Card.kt
@@ -0,0 +1,28 @@
+package fr.uge.plutus.frontend.component.common
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun Card(
+    content: @Composable () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(16.dp)
+            .clip(RoundedCornerShape(8.dp))
+            .background(MaterialTheme.colors.onSurface.copy(alpha = 0.08f))
+            .padding(16.dp)
+    ) {
+        content()
+    }
+}
