1c4c486e8f5fdb75bfa724b929d3d9d23947b1e9
ZwenDo <lorris.creantor@gmail.com>
2023-03-20 17:49:46 +0100

    Added Uri serialiazer

--- app/src/main/java/fr/uge/plutus/backend/serialization/Util.kt
+++ app/src/main/java/fr/uge/plutus/backend/serialization/Util.kt
@@ -1,5 +1,7 @@
+@file:OptIn(ExperimentalSerializationApi::class)
 package fr.uge.plutus.backend.serialization
 
+import android.net.Uri
 import kotlinx.serialization.ExperimentalSerializationApi
 import kotlinx.serialization.KSerializer
 import kotlinx.serialization.Serializer
@@ -10,7 +12,6 @@ import kotlinx.serialization.encoding.Encoder
 import java.util.*
 
 @Serializer(forClass = UUID::class)
-@OptIn(ExperimentalSerializationApi::class)
 object UUIDSerializer : KSerializer<UUID> {
 
     override val descriptor = PrimitiveSerialDescriptor("UUID", PrimitiveKind.STRING)
@@ -19,6 +20,18 @@ object UUIDSerializer : KSerializer<UUID> {
         UUID.fromString(decoder.decodeString())
 
     override fun serialize(encoder: Encoder, value: UUID): Unit =
+        encoder.encodeString(value.toString())
+
+}
+@Serializer(forClass = Uri::class)
+object UriSerializer : KSerializer<Uri> {
+
+    override val descriptor = PrimitiveSerialDescriptor("Uri", PrimitiveKind.STRING)
+
+    override fun deserialize(decoder: Decoder): Uri =
+        Uri.parse(decoder.decodeString())
+
+    override fun serialize(encoder: Encoder, value: Uri): Unit =
         encoder.encodeString(value.toString())
 
 }
