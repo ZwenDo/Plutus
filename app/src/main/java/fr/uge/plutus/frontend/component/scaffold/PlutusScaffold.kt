a1e13cbe7aa32783035d2edb83e2b3b430084821
notKamui <teillard.jimmy@outlook.fr>
2023-03-20 16:07:10 +0100

    Added auto padding to avoid the navbar hiding the content in the scaffold

--- app/src/main/java/fr/uge/plutus/frontend/component/scaffold/PlutusScaffold.kt
+++ app/src/main/java/fr/uge/plutus/frontend/component/scaffold/PlutusScaffold.kt
@@ -2,7 +2,9 @@ package fr.uge.plutus.frontend.component.scaffold
 
 import android.os.Build
 import androidx.annotation.RequiresApi
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Surface
@@ -10,6 +12,7 @@ import androidx.compose.material.rememberScaffoldState
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import fr.uge.plutus.frontend.store.initGlobalState
 import fr.uge.plutus.frontend.view.View
 
@@ -24,7 +27,11 @@ fun PlutusScaffold() {
         topBar = globalState.currentView.headerComponent,
         floatingActionButton = globalState.currentView.fabComponent,
         bottomBar = { Navbar() },
-        content = globalState.currentView.contentComponent
+        content = { padding ->
+            Column(Modifier.fillMaxSize().padding(bottom = 60.dp)) {
+                globalState.currentView.contentComponent(padding)
+            }
+        }
     )
 }
 
